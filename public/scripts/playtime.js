8a6804565ff49330f3a51e084968dfe721ebacb1
Iris van Willigen <iris.van.willigen@gmail.com>
2025-04-01 16:18:01 +0200

    Shorten playtime display

--- public/scripts/playtime.js
+++ public/scripts/playtime.js
@@ -4,5 +4,5 @@ playtimeSpans.forEach(playtimeSpan => {
   let data = playtimeSpan.innerHTML;
   let minutes = data / 60;
   let seconds = data % 60;
-  playtimeSpan.innerHTML = `${Math.floor(minutes)} min ${seconds} sec`;
+  playtimeSpan.innerHTML = `${Math.floor(minutes)} m ${seconds} s`;
 });
